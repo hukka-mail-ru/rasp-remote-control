7eeb27a775db42ec0a9021c3490146a3731375a5
Sergei <hukka@mail.ru>
2019-12-15 22:04:06 +0300

    Reader with a thread

--- reader/reader.go
+++ reader/reader.go
@@ -6,31 +6,49 @@ import (
 )
 
 func main() {
-	disk := "/dev/rfcomm0"
-	var fd, numread int
-	var err error
 
-	fd, err = syscall.Open(disk, syscall.O_RDONLY, 0777)
+	c1 := make(chan []byte)
 
-	if err != nil {
-		fmt.Print(err.Error(), "\n")
-		return
+	go func() {
+		for {
+			disk := "/dev/rfcomm0"
+			var fd, numread int
+			var err error
+
+			fd, err = syscall.Open(disk, syscall.O_RDONLY, 0777)
+
+			if err != nil {
+				fmt.Print(err.Error(), "\n")
+				return
+			}
+
+			buffer := make([]byte, 10, 100)
+
+			numread, err = syscall.Read(fd, buffer)
+
+			if err != nil {
+				fmt.Print(err.Error(), "\n")
+			}
+
+			fmt.Printf("Numbytes read: %d\n", numread)
+			fmt.Printf("Buffer: %b\n", buffer)
+
+			c1 <- buffer
+
+			err = syscall.Close(fd)
+
+			if err != nil {
+				fmt.Print(err.Error(), "\n")
+			}
+		}
+	}()
+
+	// MAIN LOOP
+	for {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		}
 	}
 
-	buffer := make([]byte, 10, 100)
-
-	numread, err = syscall.Read(fd, buffer)
-
-	if err != nil {
-		fmt.Print(err.Error(), "\n")
-	}
-
-	fmt.Printf("Numbytes read: %d\n", numread)
-	fmt.Printf("Buffer: %b\n", buffer)
-
-	err = syscall.Close(fd)
-
-	if err != nil {
-		fmt.Print(err.Error(), "\n")
-	}
 }
