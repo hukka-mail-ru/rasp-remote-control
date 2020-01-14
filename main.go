995c5695e620ebd0b85242654445a4e2aac9d737
Sergei <hukka@mail.ru>
2020-01-14 12:34:39 +0300

    print pulse space

--- main.go
+++ main.go
@@ -63,12 +63,44 @@ func convertToArrayOfUint32(bytes []byte) []uint32 {
 	for i := 0; i < len(bytes)-4; i += 4 {
 
 		res := binary.LittleEndian.Uint32(bytes[i : i+4])
-		fmt.Printf("Uint32: %08x\n", res)
 
 		array = append(array, res)
 	}
 
 	return array
+}
+
+func printPulseSpace(values []uint32) {
+
+	const PULSE_MASK = 0x01000000
+	const TIMEOUT_MASK = 0x03000000
+	const VALUE_MASK = 0x00FFFFFF
+
+	for i := 0; i < len(values); i++ {
+
+		fmt.Printf("Uint32: %08x ", values[i])
+
+		pulse := (values[i] & PULSE_MASK) > 0
+		timeout := (values[i] & TIMEOUT_MASK) > 0
+		val := values[i] & VALUE_MASK
+
+		if pulse {
+
+			fmt.Printf("pulse %d", val)
+			if val > 8000 {
+
+			}
+
+		} else if timeout {
+
+			fmt.Printf("timeout\n")
+
+		} else {
+
+			fmt.Printf("space %d\n", val)
+		}
+	}
+
 }
 
 func main() {
@@ -132,7 +164,7 @@ func main() {
 			}
 
 			arr := convertToArrayOfUint32(msg)
-			log.Info("Arr:", arr)
+			printPulseSpace(arr)
 
 			continue
 		}
